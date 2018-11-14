824ad3b826729998eeab61eb05c9b0d74ba1a571
shadowolf <shadowolfbusiness@gmail.com>
2018-11-14 13:31:20 +1100

    Added mods to the command.

--- commands/commands.js
+++ commands/commands.js
@@ -18,6 +18,7 @@ module.exports.run = async (client, message, args) => {
     .setDescription('All of the commands that I can do are listed here.')
     .addField('test', 'Just a test command :)')
     .addField('installation', 'This is a guide that helps users install the Modpack via the Technic Launcher.')
+    .addField('mods', 'Provides the user a link to the Harmony Hollow mod list.')
   message.channel.send(embed);
   return
 };
