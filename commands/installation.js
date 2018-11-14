63ec8aff19323dad3f898feed77c1d8ada1ec485
shadowolf <shadowolfbusiness@gmail.com>
2018-11-14 14:18:48 +1100

    Update installation.js

--- commands/installation.js
+++ commands/installation.js
@@ -15,6 +15,7 @@ module.exports.run = async (client, message, args) => {
 
   let embed = new Discord.RichEmbed()
     .setTitle('Modpack Installation Guide')
+    .setThumbnail('https://theme.zdassets.com/theme_assets/646263/b26a053b1a803ec41d3aa316adb534b82442757f.png')
     .setDescription('1. Download the Technic Launcher [https://www.technicpack.net/download] and install it.\n2. Enter in your Minecraft Account details and login to the launcher.\n3. At the top of the launcher select the Modpacks tab.\n4. At the top of the window type in the text box: Harmony Hollow.\n5. Select Install and wait until Modpack is installed and the button has been changed to Play.\n6. When the Modpack is installed and the button displays Play, press Play to launch the Harmony Hollow Modpack.')
   message.channel.send(embed);
   return
