843bc90c4cb92dc1344a570fc25075cac75b963f
shadowolf <shadowolfbusiness@gmail.com>
2018-11-14 13:30:08 +1100

    Added mods command.

---
+++ commands/mods.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const errors = require('../util/errors.js');
+
+module.exports.run = async (client, message, args) => {
+  if (args == 'help') {
+    let embed = new Discord.RichEmbed()
+      .setTitle(`${module.exports.help.name} Command Information`)
+      .setDescription(`${module.exports.help.description}`)
+      .addField('Usage', `${config.prefix}${module.exports.help.usage}`)
+      .setColor(config.cyan)
+    message.channel.send(embed);
+    return
+  };
+
+  let embed = new Discord.RichEmbed()
+    .setTitle('View the Mod List here')
+    .setURL('https://docs.google.com/spreadsheets/d/1655brAQn6xwUzrR6t3OXAhC_jpuQtjAIvJN6C9dnOJw/edit#gid=0')
+    .setDescription('Clicking the blue link will take you to the Harmony Hollow Spreadsheet where you will find all of the mods in the modpack.')
+  message.channel.send(embed);
+  return
+};
+
+module.exports.help = {
+  name: 'mods',
+  description: 'Gives the user a link to the Harmony Hollow Mod List',
+  usage: `mods`
+};
